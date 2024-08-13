fba6b0ab34d078ef36c567d26ebfef9716237978
Lawrence L <LawrenceELee@users.noreply.github.com>
2024-08-12 20:59:16 -0700

    Finished returning tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,25 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   // use destructuring to extract name, email, and password from req.body
+  /*
   const { name, email, password } = req.body;
 
-  // validate user input includes name, email, and password
-  /*
-  if (!name || !email || !password) {
-    throw new BadRequestError("Please provide name, email, and password");
-  }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const tempUser = { name, email, password: hashedPassword };
   */
 
+  // we handle the salt and hash in the model using a pre-save hook
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 
   //res.send("register user");
 };
