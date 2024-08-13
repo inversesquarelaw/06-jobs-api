4a94a17924be88c6bf80f995373352a75e4802c9
Lawrence L <LawrenceELee@users.noreply.github.com>
2024-08-12 20:31:48 -0700

    Manually checking the validation, but we can remove lines 10 to 12 and mongoose will do the validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,18 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
 
 const register = async (req, res) => {
+  // use destructuring to extract name, email, and password from req.body
+  const { name, email, password } = req.body;
+
+  // validate user input includes name, email, and password
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please provide name, email, and password");
+  }
+
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 
   //res.send("register user");
 };
